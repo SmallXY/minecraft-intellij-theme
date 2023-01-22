c04baf21fd852a323d750965d76721f0762ef1ca
GitHub Action <action@github.com>
2023-01-22 09:38:03 +0000

    Template cleanup

--- src/main/kotlin/com/github/smallxy/minecraftintellijtheme/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/smallxy/minecraftintellijtheme/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.smallxy.minecraftintellijtheme.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.smallxy.minecraftintellijtheme.MyBundle
 
 class MyApplicationService {
 
